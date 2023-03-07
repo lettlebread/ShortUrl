b1eb25f2b22418016d6cd695045728b5c51f08bc
johnny.chen <johnny.chen@thekono.com>
2023-03-07 21:27:32 +0800

    feat(api): add user login api in route "[post] /api/user/login"

---
+++ src/pages/api/user/login.ts
@@ -0,0 +1,51 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from "@/libs/dbService"
+import { isString } from '@/libs/stringUtil'
+import { ApiError } from '@/interfaces/request'
+import { compareWithHash } from '@/libs/bycryptUtil'
+import isemail from "isemail"
+import { errorWrapper } from '@/middleware/errorWrapper'
+
+const postHandler = async(req: NextApiRequest, res: NextApiResponse ) => {
+  try {
+    const { email, password } = req.body
+
+    if (!isString(email) ||
+      !isString(password)
+      ) {
+      throw new ApiError(400, "invalid argument")
+    }
+
+    const user = await prisma?.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user || !compareWithHash(password, user.password)) {
+      throw new ApiError(401, "invalid email or password")
+    }
+
+    res.status(200).json({});
+  } catch(e: any) {
+    if (typeof e?.code === "string" && e?.code.startsWith("P")) {
+      throw new ApiError(404, "fail to create user")
+    } else {
+      throw e
+    }
+  }
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === "POST") {
+      await postHandler(req, res);
+    } else {
+      res.status(404).json({});
+    }
+  } catch (e) {
+    throw e;
+  }
+};
+
+export default errorWrapper(handler);

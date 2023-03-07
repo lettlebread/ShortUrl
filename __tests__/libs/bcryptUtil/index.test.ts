f7dd13ce4efe8d07fb9ff4e014429d22db0b334d
johnny.chen <johnny.chen@thekono.com>
2023-03-07 20:41:34 +0800

    feat(api): add user register api in route "[post] /api/user/register"
    Add user schema in schema.prisma.
    
    Add test case for compareWithHash in bcryptUtil.

--- __tests__/libs/bcryptUtil/index.test.ts
+++ __tests__/libs/bcryptUtil/index.test.ts
@@ -1,7 +1,11 @@
 import { compareWithHash } from '@/libs/bycryptUtil'
 
 describe('encryptString test', () => {
-  it('compare hash', () => {
+  it('compare with correct hash', () => {
     expect(compareWithHash("testpassword", "$2b$10$nfAx05UdLjmpVMq2ptVLfeQRqgMmeIdX97urYlmYW9OekUTMJ0hyi")).toBe(true)
+  })
+
+  it('compare with incorrect hash', () => {
+    expect(compareWithHash("testpassword", "$2b$10$nfAx05UdLjmpVMq2ptVLfeQRqgMmeIdX97urYlmYW9OekUTMJ0hy_")).toBe(false)
   })
 })
---
+++ src/pages/api/user/register.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from "@/libs/dbService"
+import { isString } from '@/libs/stringUtil'
+import { ApiError } from '@/interfaces/request'
+import { encryptString } from '@/libs/bycryptUtil'
+import isemail from "isemail"
+import { errorWrapper } from '@/middleware/errorWrapper'
+
+const postHandler = async(req: NextApiRequest, res: NextApiResponse ) => {
+  try {
+    const { email, password } = req.body
+
+    if (!isString(email) ||
+      !isString(password) ||
+      !isemail.validate(email)
+      ) {
+      throw new ApiError(400, "invalid argument")
+    }
+
+    await prisma?.user.create({
+      data: {
+        email,
+        password: encryptString(password)
+      },
+    })
+
+    res.status(200).json({});
+  } catch(e: any) {
+    if (typeof e?.code === "string" && e?.code.startsWith("P")) {
+      throw new ApiError(404, "fail to create user")
+    } else {
+      throw e
+    }
+  }
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === "POST") {
+      await postHandler(req, res);
+    } else {
+      res.status(404).json({});
+    }
+  } catch (e) {
+    throw e;
+  }
+};
+
+export default errorWrapper(handler);
